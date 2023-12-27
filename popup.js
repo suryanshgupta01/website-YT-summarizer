b18a9a612dde01f7b5794fc913d7d9961c3c1c3e
suryansh <suryanshgupta03@gmail.com>
2023-12-27 14:17:15 +0530

    first commit

---
+++ popup.js
@@ -0,0 +1,64 @@
+const textContainer = document.getElementById("text-container");
+document.addEventListener("DOMContentLoaded", function () {
+    const text = "This is not an example text. Welcome to Text Animation! \
+    Lorem ipsum dolor sit amet consectetur adipisicing elit.Enim\
+     architecto repudiandae repellat, ducimus eligenditationem sit!";
+    let index = 0;
+
+    function updateText() {
+        if (index < text.length) {
+            //             textContainer.removeChild(textContainer.children[1])
+            const span = document.createElement("span");
+            span.textContent = text[index];
+            textContainer.appendChild(span);
+
+            const cursor = document.createElement("div");
+            cursor.classList.add("cursor");
+            textContainer.appendChild(cursor);
+
+            index++;
+            // Remove the cursor after a short delay (adjust as needed)
+            setTimeout(() => {
+                textContainer.removeChild(cursor);
+            }, 50);
+            requestAnimationFrame(updateText);
+        }
+    }
+
+    // updateText();
+    // textContainer.innerHTML = text;
+});
+const baseURL ="http://127.0.0.1:5000"
+execute_gemini = (website) => {
+    var url = baseURL + "/fetch_gemini"
+    var formData = new FormData();
+    formData.append('url', website);
+    textContainer.innerHTML = "LOADING...";
+
+        fetch(url, {
+            method: 'POST',
+            body: formData
+        })
+        .then(function (response) {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(function (data) {
+            // console.log(data)
+            textContainer.innerHTML = data.gemini_response;
+        }).catch(function (error) {
+            console.log('There was a problem with the fetch operation:', error);
+        });
+
+};
+
+console.log("hello")
+chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    var tab = tabs[0];
+    var url = tab.url;
+    const ele = document.getElementById('newid');
+    // ele.innerHTML = url;
+    execute_gemini(url)
+});
---
+++ transcript.py
@@ -0,0 +1,39 @@
+from youtube_transcript_api import YouTubeTranscriptApi
+import google.generativeai as genai
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY', "")
+genai.configure(api_key=GOOGLE_API_KEY)
+
+app = Flask(__name__)
+CORS(app)
+
+genai.configure(api_key=GOOGLE_API_KEY)
+model = genai.GenerativeModel('gemini-pro')
+
+@app.route('/fetch_gemini', methods=['POST'])
+def fetch_gemini():
+    url = request.form['url']
+    isYoutube = url.startswith("https://www.youtube.com/watch?v=")
+    link=url[32:43]
+    if(isYoutube):
+        transcript=""
+        x = YouTubeTranscriptApi.get_transcript(link,languages=['en','hi'])
+        for i in range(len(x)):
+            transcript += x[i]["text"]+" "
+        response = model.generate_content("Tell the key points in this video from the transcript given below"+transcript)
+    else:
+        response = model.generate_content("Summarize the content of the given website in key points "+url)
+    answer = response.text
+    return jsonify({'gemini_response':answer})
+
+
+# for development only
+# if __name__ == "__main__":
+#     print("Starting Python Flask Server For Summarizing Website and YT videos")
+#     app.run(debug=True)
+
